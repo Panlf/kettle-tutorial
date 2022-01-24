f4416f6dd2131b742c324e9c6ec84fe755fca0e6
Panlf <1805893180@qq.com>
2022-01-24 15:40:54 +0800

    add job manage

--- src/main/java/com/plf/kettle/web/DispatchKettleWeb.java
+++ src/main/java/com/plf/kettle/web/DispatchKettleWeb.java
@@ -4,6 +4,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pentaho.di.cluster.SlaveServer;
 import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.job.Job;
+import org.pentaho.di.job.JobExecutionConfiguration;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.entries.special.JobEntrySpecial;
+import org.pentaho.di.job.entry.JobEntryCopy;
+import org.pentaho.di.repository.RepositoryDirectoryInterface;
+import org.pentaho.di.repository.filerep.KettleFileRepository;
+import org.pentaho.di.repository.filerep.KettleFileRepositoryMeta;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransExecutionConfiguration;
 import org.pentaho.di.trans.TransMeta;
@@ -14,6 +23,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 public class DispatchKettleWeb {
 
@@ -39,7 +49,83 @@ public class DispatchKettleWeb {
         }
     }
 
+    /**
+     * 将KJB内容发布到kettle服务器上执行。
+     * @param jobName
+     * @return
+     * @throws Exception
+     */
+    public String startJob(String jobName) throws Exception{
+        try {
+            String id = "", name = "", description = "无描述";
+            // Job文件存放路径
+            String localKettleBaseDirectory =  "/KettleHome";
+            KettleFileRepositoryMeta repInfo = new KettleFileRepositoryMeta(id,
+                    name, description, localKettleBaseDirectory);
+            // 文件形式的资源库
+            KettleFileRepository rep = new KettleFileRepository();
+            rep.init(repInfo);
+            RepositoryDirectoryInterface tree = rep.loadRepositoryDirectoryTree();
+            RepositoryDirectoryInterface rd = tree.findDirectory("");
 
+            JobMeta jobMeta = rep.loadJob(jobName, rd, null, null); // jobName = jobName[.kjb]
+
+//			JobMeta jobMeta = new JobMeta(inputStream, null, null);
+            JobExecutionConfiguration jobExecutionConfiguration = new JobExecutionConfiguration();
+            jobExecutionConfiguration.setRemoteServer(slaveServer);
+
+            jobExecutionConfiguration.setExecutingLocally(false);
+            jobExecutionConfiguration.setExecutingRemotely(true);
+            jobExecutionConfiguration.setPassingExport(false);
+            jobExecutionConfiguration.setRepository(rep);
+            //设置全局参数 此处的Variables 会被JobMeta的Variables覆盖 详见源代码
+//			jobExecutionConfiguration.setVariables(variablesMap);
+            return Job.sendToSlaveServer(jobMeta, jobExecutionConfiguration, null, null);
+        } catch (KettleXMLException e) {
+            return null;
+        }
+    }
+    /**
+     * 开始job作业
+     * @param in kjb文本
+     * @return kettle执行kjb的作业Id
+     */
+    public String startJob(InputStream in) throws Exception{
+        try {
+            // 配置任务信息
+            JobMeta meta = new JobMeta(in, null,null);
+            JobExecutionConfiguration jobExecutionConfiguration = new JobExecutionConfiguration();
+            jobExecutionConfiguration.setRemoteServer(slaveServer);// 配置远程服务
+            // 需要判断是否在执行,否则每执行一次就会产生一个lastCarteObjectId
+            String carteObjectId = Job.sendToSlaveServer(meta, jobExecutionConfiguration, null, null);
+            return carteObjectId;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    /**
+     * 开始job作业  带环境变量
+     * @param in kjb文本
+     * @return kettle执行kjb的作业Id
+     */
+    public String startJob(InputStream in, Map<String, String> variables, SlaveServer server) throws Exception{
+        try {
+            // 配置任务信息
+            JobMeta meta = new JobMeta(in, null, null);
+            JobExecutionConfiguration jobExecutionConfiguration = new JobExecutionConfiguration();
+            jobExecutionConfiguration.setRemoteServer(server);// 配置远程服务
+            jobExecutionConfiguration.setVariables(variables);// 设置环境变量
+            // 需要判断是否在执行,否则每执行一次就会产生一个lastCarteObjectId
+            String carteObjectId = Job.sendToSlaveServer(meta, jobExecutionConfiguration, null, null);
+            JobEntryCopy startPoint =  meta.findJobEntry(JobMeta.STRING_SPECIAL_START);
+            JobEntrySpecial jes = (JobEntrySpecial) startPoint.getEntry();
+            boolean isRepeat = jes.isRepeat();	//判断是否为任务是否设置了重复
+            System.out.println("任务是否设置了重复:"+isRepeat);
+            return carteObjectId;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 
 
     /**
@@ -59,9 +145,6 @@ public class DispatchKettleWeb {
             e.printStackTrace();
         }
     }
-
-
-
 
     /**
      * 停止作业
